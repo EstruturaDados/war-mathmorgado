a765362f2cd23544f47e7c7e9e75644dc8a47042
Matheus Morgado <94700017+mathmorgado@users.noreply.github.com>
2025-10-30 00:06:51 +0000

    Desafio aventureiro completo

--- war_aventureiro.c
+++ war_aventureiro.c
@@ -1,11 +1,12 @@
 // Inclusão das bibliotecas
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 // --- Constantes Globais ---
 #define MAX_TAM_STRING 50
-#define MAX_TERRITORIOS 5
+#define MAX_TERRITORIOS 3
 
 
 // --- Estrutura de Dados ---
@@ -18,11 +19,14 @@ typedef struct
 
 
 // declarando as funções
-void exibir_mapa(war_game territorios[]);
 void limparBufferEntrada();
+void exibir_mapa(war_game territorios[]);
+void fase_ataque(war_game territorios[]);
+void simular_ataque(war_game territorios[], int atacante, int defensor);
 
 // --- Função Principal (main) ---
-int main() {
+int main() 
+{
     war_game *territorios;
     territorios = calloc(MAX_TERRITORIOS, sizeof(war_game));
     
@@ -47,13 +51,18 @@ int main() {
     }
 
     exibir_mapa(territorios);
+    fase_ataque(territorios);
+    
+    free(territorios);
+    printf("\nJogo encerrado e memoria liberada. Até a proxima!\n");
     
     return 0;
 }
 
+
 void exibir_mapa(war_game territorios[]) 
 {
-    printf("========================================\n");
+    printf("\n========================================\n");
     printf("      MAPA DO MUNDO - ESTADO ATUAL      \n");
     printf("========================================\n");
 
@@ -70,8 +79,86 @@ void exibir_mapa(war_game territorios[])
     printf("\n");
 }
 
+
+void fase_ataque(war_game territorios[]) 
+{
+    int opcao_atacante;
+    int opcao_defesa;
+
+    while (1)
+    {
+        printf("\n--- FASE DE ATAQUE --\n");
+        
+        printf("Escolha o territorio atacante (1 a %d, ou 0 para sair): ", MAX_TERRITORIOS);
+        scanf("%d", &opcao_atacante);
+        limparBufferEntrada();
+
+        if (opcao_atacante == 0)
+        {
+            break;
+        }
+
+        printf("Escolha o territorio defensor (1 a %d): ", MAX_TERRITORIOS);
+        scanf("%d", &opcao_defesa);
+        limparBufferEntrada();
+
+        simular_ataque(territorios, opcao_atacante - 1, opcao_defesa - 1);        
+    }
+
+}
+
+
+void simular_ataque(war_game territorios[], int atacante, int defensor) 
+{
+    int dado_atacante = rand() % 6 + 1;  // Gera número de 1 a 6
+    int dado_defensor = rand() % 6 + 1;  // Gera número de 1 a 6
+
+    printf("\n--- RESULTADO DA BATALHA ---\n");
+    printf("O atacante %s rolou um dado e tirou: %d\n", territorios[atacante].nome, dado_atacante);
+    printf("O defensor %s rolou um dado e tirou: %d\n", territorios[defensor].nome, dado_defensor);
+
+    if (dado_atacante > dado_defensor)
+    {
+        printf("VITÓRIA DO ATAQUE! O defensor perdeu 1 tropa.\n");
+        territorios[defensor].tropas--;
+
+        if (territorios[defensor].tropas == 0)
+        {
+            printf("CONQUISTA! O territorio %s foi dominado pelo Exercito %s!\n", territorios[defensor].nome, territorios[atacante].cor);
+            strcpy(territorios[defensor].cor, territorios[atacante].cor);
+            territorios[defensor].tropas = 1;
+        }
+        
+    }
+    else if (dado_atacante < dado_defensor)
+    {
+        printf("VITÓRIA DA DEFESA! O atacante perdeu 1 tropa.\n");
+        territorios[atacante].tropas--;
+
+        if (territorios[atacante].tropas == 0)
+        {
+            printf("CONQUISTA! O territorio %s foi dominado pelo Exercito %s!\n", territorios[atacante].nome, territorios[defensor].cor);
+            strcpy(territorios[atacante].cor, territorios[defensor].cor);
+            territorios[atacante].tropas = 1;
+        }
+    }
+    else 
+    {
+        printf("EMPATE! Ninguém perdeu tropas.\n");
+    }
+
+    printf("\nPressione 'Enter' para continuar...");
+    limparBufferEntrada();
+    getchar();  // Aguarda o Enter
+
+    exibir_mapa(territorios);
+}
+
+
+
 // Função utilitária para limpar o buffer de entrada do teclado (stdin), evitando problemas com leituras consecutivas de scanf e getchar.
-void limparBufferEntrada() {
+void limparBufferEntrada()
+{
     int c;
     while((c = getchar()) != '\n' && c != EOF);
 }
